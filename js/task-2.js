2b879b811320bb2a8be745e620308cc0928f934e
BonakUA <bonak79@gmail.com>
2024-02-24 22:36:39 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,4 @@
+"use script";
 function getShippingMessage(country, price, deliveryFee) {
     const totalPrice = price + deliveryFee;
     return `Shipping to ${country} will cost ${totalPrice} credits`
